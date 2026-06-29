09e0dcde6fb3e6cb47c4a6d537a139b7146904e4
Bünyamin Epatay <epatayb@gmail.com>
2026-06-29 03:06:33 +0300

    index action tasarlandı.

--- GezginTravel/Controllers/Admin/AdminCategoryController.cs
+++ GezginTravel/Controllers/Admin/AdminCategoryController.cs
@@ -1,7 +1,11 @@
 ﻿using GezginTravel.Constants;
 using GezginTravel.Data;
+using GezginTravel.Models.Entities;
+using GezginTravel.ViewModels.Dashboard.Admin.AdminCategory;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace GezginTravel.Controllers.Admin
 {
@@ -17,10 +21,91 @@ namespace GezginTravel.Controllers.Admin
         }
 
         [HttpGet("")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index(
+            string? searchText,
+            string status = "Active",
+            string? sortBy = "created_desc",
+            int page = 1)
+
         {
-            return View();
+            const int pageSize = 10;
+
+            if (page < 1)
+            {
+                page = 1;
+            }
+
+            var query = _context.Categories
+                .Include(x => x.BlogCategories)
+                .AsQueryable();
+
+            status = string.IsNullOrWhiteSpace(status)
+                ? "Active"
+                : status;
+
+            query = status switch
+            {
+                "Deleted" => query.Where(x => x.IsDeleted),
+                "all" => query,
+                _ => query.Where(x => !x.IsDeleted),
+            };
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                var search = searchText.Trim();
+                var likeSearch = $"%{search}%";
+
+                query = query.Where(x =>
+                    EF.Functions.Like(x.Name, likeSearch) ||
+                    EF.Functions.Like(x.Slug, likeSearch));
+            }
+
+            query = ApplySorting(query, sortBy);
+
+            var totalItems = await query.CountAsync();
+
+            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            if (totalPages > 0 && page > totalPages)
+            {
+                page = totalPages;
+            }
+
+            var categories = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(x => new AdminCategoryListItemViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Slug = x.Slug,
+                    BlogCount = x.BlogCategories.Count,
+                    CreatedDate = x.CreatedDate,
+                    UpdatedDate = x.UpdatedDate,
+                    IsDeleted = x.IsDeleted
+                })
+                .ToListAsync();
+
+            var model = new AdminCategoryIndexViewModel
+            {
+                SearchText = searchText,
+                Status = status,
+                SortBy = sortBy,
+
+                TotalCategories = await _context.Categories.CountAsync(),
+                ActiveCategories = await _context.Categories.CountAsync(x => !x.IsDeleted),
+                DeletedCategories = await _context.Categories.CountAsync(x => x.IsDeleted),
+                UsedCategories = await _context.Categories.CountAsync(x => x.BlogCategories.Any()),
+
+                Categories = categories,
+
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+            };
+            return View(model);
         }
+
         [HttpGet("create")]
         public IActionResult Create()
         {
@@ -29,16 +114,9 @@ namespace GezginTravel.Controllers.Admin
 
         [HttpPost("create")]
         public IActionResult Create(int Id)
-        { 
-            return View(); 
+        {
+            return View();
         }
-
-        //[HttpPost("create")]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult CreatePost()
-        //{
-        //    return RedirectToAction(nameof(Index));
-        //}
 
         [HttpGet("edit/{id:int}")]
         public IActionResult Edit(int id)
@@ -59,5 +137,25 @@ namespace GezginTravel.Controllers.Admin
         {
             return RedirectToAction(nameof(Index));
         }
+
+        private static IQueryable<Category> ApplySorting(IQueryable<Category> query, string? sortBy)
+        {
+            return sortBy switch
+            {
+                "name_asc" => query.OrderBy(x => x.Name),
+                "name_desc" => query.OrderByDescending(x => x.Name),
+
+                "blog_asc" => query.OrderBy(x => x.BlogCategories.Count).ThenBy(x => x.Name),
+                "blog_desc" => query.OrderByDescending(x => x.BlogCategories.Count).ThenBy(x => x.Name),
+
+                "updated_asc" => query.OrderBy(x => x.UpdatedDate ?? x.CreatedDate),
+                "updated_desc" => query.OrderByDescending(x => x.UpdatedDate ?? x.CreatedDate),
+
+                "created_asc" => query.OrderBy(x => x.CreatedDate),
+
+                _ => query.OrderByDescending(x => x.CreatedDate),
+            };
+        }
+
     }
 }
