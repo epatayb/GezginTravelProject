57cccc8179751fb26075cb7dc7d9612f3801ba69
Bünyamin Epatay <epatayb@gmail.com>
2026-07-28 05:30:13 +0300

    updated

---
+++ GezginTravel/ViewModels/Dashboard/Admin/AdminLocation/AdminCityCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GezginTravel.ViewModels.Dashboard.Admin.AdminLocation
+{
+    public class AdminCityCreateViewModel
+    {
+        [Required(ErrorMessage = "Şehir adı zorunludur.")]
+        [StringLength(50, ErrorMessage = "Şehir adı en fazla 50 karakter olabilir.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Ülke seçimi zorunludur.")]
+        public int? CountryId { get; set; }
+
+        public List<SelectListItem> CountryOptions { get; set; } = new();
+    }
+}
