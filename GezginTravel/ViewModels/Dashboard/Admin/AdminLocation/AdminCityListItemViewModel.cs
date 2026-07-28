4e705865e55ced9b69f45b72ed70b5e33f10ccc0
Bünyamin Epatay <epatayb@gmail.com>
2026-07-28 04:07:41 +0300

    updated

---
+++ GezginTravel/ViewModels/Dashboard/Admin/AdminLocation/AdminCityListItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace GezginTravel.ViewModels.Dashboard.Admin.AdminLocation
+{
+    public class AdminCityListItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+
+        public int CountryId { get; set; }
+        public string CountryName { get; set; } = string.Empty;
+
+        public int BlogCount { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public bool IsCountryDeleted { get; set; }
+    }
+}
---
+++ GezginTravel/ViewModels/Dashboard/Admin/AdminLocation/AdminCountryListItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace GezginTravel.ViewModels.Dashboard.Admin.AdminLocation
+{
+    public class AdminCountryListItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+
+        public int CityCount { get; set; }
+        public int BlogCount { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
