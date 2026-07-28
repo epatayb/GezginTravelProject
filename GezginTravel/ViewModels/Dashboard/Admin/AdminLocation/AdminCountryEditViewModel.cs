547ce8db2070d517c87fc762082694b70f3d3849
Bünyamin Epatay <epatayb@gmail.com>
2026-07-28 05:30:25 +0300

    updated

---
+++ GezginTravel/ViewModels/Dashboard/Admin/AdminLocation/AdminCountryEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GezginTravel.ViewModels.Dashboard.Admin.AdminLocation
+{
+    public class AdminCountryEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ülke adı zorunludur.")]
+        [StringLength(30, ErrorMessage = "Ülke adı en fazla 30 karakter olabilir.")]
+        public string Name { get; set; } = string.Empty;
+
+        public string Slug { get; set; } = string.Empty;
+    }
+}
